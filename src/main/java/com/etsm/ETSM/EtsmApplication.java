4661363a51d909a3a6dcd74776c361be0310111f
dart647 <spivak1999@yandex.ru>
2019-12-01 20:21:34 +0300

    Create new project

---
+++ src/main/java/com/etsm/ETSM/EtsmApplication.java
@@ -0,0 +1,13 @@
+package com.etsm.ETSM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtsmApplication.class, args);
+	}
+
+}
